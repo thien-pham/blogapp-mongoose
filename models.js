bf5ad54e918cde45f44c5794880f2e105cd46726
pham-thien <pham.thien@outlook.com>
2017-05-02 14:43:44 -0400

    add schema and first get

---
+++ models.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+    title: {type: String, required: true},
+    content: {type: String, required: true},
+    author: {
+        firstName: String,
+        lastName: String
+    }
+});
+
+postSchema.virtual('authorName').get(function(){
+    return `${this.author.firstName} ${this.author.lastName}`;
+});
+
+postSchema.methods.apiRepr = function() {
+    return {
+        id: this._id,
+        title: this.title,
+        content: this.content,
+        author: this.authorName
+    };
+};
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = {Post};
--- server.js
+++ server.js
@@ -11,7 +11,21 @@ const {Posts} = require('./models');
 const app = express();
 app.use(bodyParser.json());
 
-
+app.get('/posts', (req, res) => {
+    Post
+        .find()
+        .exec()
+        .then(posts => {
+            res.json({
+                posts: posts.map((post) => post.apiRepr())
+            });
+        })
+        .catch(
+            err => {
+                console.error(err);
+                res.status(500).json({message: 'Error!'});
+            });
+});
 
 
 
@@ -20,7 +34,6 @@ app.use(bodyParser.json());
 
 let server;
 
-// this function connects to our database, then starts the server
 function runServer(databaseUrl=DATABASE_URL, port=PORT) {
 
   return new Promise((resolve, reject) => {
